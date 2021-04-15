fc25f2bdad467573cf15532e877923f4ecc4e9ce
Grantk2401 <grk1994@hotmail.co.uk>
2021-04-15 17:34:28 +0100

    Added custom functions to functions.py

--- Project/functions.py
+++ Project/functions.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 general = pd.read_csv("general_data.csv")
 survey = pd.read_csv("employee_survey_data.csv")
@@ -21,3 +22,42 @@ def importsurvey(filename):
     print(survey.index)
     print(survey.info)
 
+def runaverages():
+    average_satisfaction = np.mean(merged_satisfaction["JobSatisfaction"])
+    average_age = np.mean(merged_satisfaction["Age"])
+    average_years = np.mean(merged_satisfaction["YearsAtCompany"])
+    average_distance = np.mean(merged_satisfaction["DistanceFromHome"])
+
+    print(average_satisfaction, average_age, average_years, average_distance)
+
+def addfiles():
+    general = pd.read_csv("general_data.csv")
+    survey = pd.read_csv("employee_survey_data.csv")
+
+    # data cleaning
+    print(general.isnull().sum())
+    print(survey.isnull().sum())
+
+    general.fillna(0, inplace=True)
+    survey.fillna(0, inplace=True)
+
+    print(general.isnull().any())
+    print(survey.isnull().any())
+
+    # sorting
+    general_years = general.sort_values("YearsAtCompany", ascending=False)
+    survey_satisfaction = survey.sort_values("JobSatisfaction", ascending=False)
+
+    print(general_years)
+    print(survey_satisfaction)
+
+    # looping iterrows
+    for lab, row in general_years.iterrows():
+        print("EmployeeID:", lab, "- ", row["YearsAtCompany"])
+
+    # merging dataframes
+    merged_data = pd.merge(general_years, survey_satisfaction, on="EmployeeID")
+    print(merged_data.info)
+
+    merged_satisfaction = merged_data.sort_values("JobSatisfaction", ascending=False)
+    print(merged_satisfaction)
